65746e3215eefef0e97e0185c047f8d3de4d7e7c
amanagarwal09 <62548194+amanagarwal09@users.noreply.github.com>
2020-03-23 17:19:17 +0530

    Add files via upload

---
+++ assignment-master/project.sql
@@ -0,0 +1,13 @@
+create database today20_03_2020;
+
+use today20_03_2020;
+
+create table student(id int not null,name varchar(50) not null,marks int not null);
+
+drop table student;
+
+select * from student;
+
+insert into Student values(1,"Avik",80);
+
+TRUNCATE TABLE student;
